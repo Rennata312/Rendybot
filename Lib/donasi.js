814bc2488cce0025189a011a57ead03e8ca29769
Rennata312 <154791203+Rennata312@users.noreply.github.com>
2024-01-19 00:33:20 +0700

    Add files via upload

---
+++ Lib/donasi.js
@@ -0,0 +1,13 @@
+const donasi = () => { 
+	return `       
+
+◪ *DONASI*
+  │
+  ├─ ❏ *GOPAY*
+  ├─ ❏ 082287486762
+  ├─ ❏ *PULSA*
+  ├─ ❏ 082287486762
+  ├─ ❏ DONASI KAK BUAT UPDATE BOT NYA`
+}
+
+exports.donasi = donasi
